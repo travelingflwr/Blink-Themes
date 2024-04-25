97a86bbfa257b8c64718cbef3f86193158eb0f82
Amy Devon <travelingflwr@icloud.com>
2024-04-24 17:46:50 -0700

    fixed cursor for astro

--- blink-astro-theme/astro-theme-dark.js
+++ blink-astro-theme/astro-theme-dark.js
@@ -1,4 +1,4 @@
 t.prefs_.set("color-palette-overrides",["#15161e", "#F8747E", "#75AD47", "#D09214", "#50A4E9", "#CC83E3", "#00B298", "#a9b1d6", "#414868", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#c0caf5"]);
 t.prefs_.set("foreground-color", "#c0caf5");
 t.prefs_.set("background-color", "#1A1D23");
-t.prefs_.set("cursor-color", "#21242A");
+t.prefs_.set("cursor-color", "#rgba(0, 0, 0, 0.5)21242A");
